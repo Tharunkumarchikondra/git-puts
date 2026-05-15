08e4be05456f1d60b6067b4f0998094a9a80d987
Tharun Kumar <chikondratharunkumar>
2026-05-15 17:07:34 +0530

    Added new code file

--- code1.java
+++ code1.java
@@ -1,3 +1,7 @@
-int a=10;
-int b=20;
-System.oout.println(a+b);
+public class code1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+    }
+}
